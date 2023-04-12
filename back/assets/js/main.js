7a7c6bc41a401c2cab2a91bd95ce4526b9417ba7
Emilia Salazar <emiliasalazar67@gmail.com>
2023-04-12 11:58:26 -0600

    landing-first-drafy

---
+++ back/assets/js/main.js
@@ -0,0 +1,40 @@
+// Existing code from landing.js
+setTimeout(function () {
+  anime.timeline()
+    .add({
+      targets: "#grid-container-item1 h1 .word",
+      scale: [14, 1],
+      opacity: [0, 1],
+      easing: "easeOutCirc",
+      duration: 800,
+      delay: (el, i) => 800 * i
+    })
+    .add({
+      targets: "#grid-container-item2 h3 .word",
+      scale: [14, 1],
+      opacity: [0, 1],
+      easing: "easeOutCirc",
+      duration: 800,
+      delay: (el, i) => 800 * i
+    });
+}, 2000);
+
+// Function to redirect to the index page after a specified duration
+function redirectToIndex() {
+  setTimeout(function () {
+    $('body').fadeOut(1000, function () {
+      window.location.href = "index.html";
+    });
+  }, 9000); // Change the value to adjust the duration (in milliseconds) before redirection
+}
+
+
+// Document ready function
+$(document).ready(function () {
+  // ... (other existing code, if any)
+
+  redirectToIndex();
+});
+
+// Existing code from index.js (if any)
+// ...
