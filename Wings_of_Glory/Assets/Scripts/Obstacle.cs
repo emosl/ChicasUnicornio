68bca15d5430fe2f3f2087ccb6160c962fdf0d8b
Fernanda Osorio <A01026502@tec.mx>
2023-04-17 23:32:57 -0600

    obstacle new

--- Wings_of_Glory/Assets/Scripts/Obstacle.cs
+++ Wings_of_Glory/Assets/Scripts/Obstacle.cs
@@ -52,6 +52,77 @@
 // }
 
 
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Obstacle : MonoBehaviour
+// {
+//     private Collider2D obstacleCollider;
+//     public GameObject canvas;
+//     private Preguntas preguntas;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         obstacleCollider = GetComponent<Collider2D>();
+//         preguntas = canvas.GetComponent<Preguntas>();
+//         preguntas.ButtonPressed += OnTriggerDecision;
+        
+//     }
+
+//     public void AskPermission()
+//     {
+//         preguntas.ShowQuestion("Do you want to go through this obstacle?", OnTriggerDecision);
+//     }
+
+//     private void OnTriggerDecision(bool shouldTrigger)
+//     {
+//         if (shouldTrigger)
+//         {
+//             MakeTriggersForObstacleColliders();
+//         }
+//         else
+//         {
+//             TurnTriggerIntoCollider();
+//         }
+//     }
+
+//     public void MakeTriggersForObstacleColliders()
+//     {
+//         GameObject[] obstacleColliders = GameObject.FindGameObjectsWithTag("Obstacle_Collider");
+//         foreach (GameObject obstacleColliderObject in obstacleColliders)
+//         {
+//             Collider2D collider = obstacleColliderObject.GetComponent<Collider2D>();
+//             if (collider != null)
+//             {
+//                 collider.isTrigger = true;
+//             }
+//         }
+//         canvas.GetComponent<Preguntas>().HideQuestion();
+//     }
+
+//     private void TurnTriggerIntoCollider()
+//     {
+//         obstacleCollider.isTrigger = false;
+//         canvas.GetComponent<Preguntas>().HideQuestion();
+//     }
+
+//     void OnDestroy()
+//     {
+//         // Unsubscribe the event when the object is destroyed
+//         if (preguntas != null)
+//         {
+//             preguntas.ButtonPressed -= OnTriggerDecision;
+//         }
+//     }
+//     public void Restart()
+//     {
+//         obstacleCollider.isTrigger = true;
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -79,7 +150,7 @@ public class Obstacle : MonoBehaviour
     {
         if (shouldTrigger)
         {
-            MakeTriggersForObstacleColliders();
+            MakeTriggerForCurrentCollider();
         }
         else
         {
@@ -87,23 +158,39 @@ public class Obstacle : MonoBehaviour
         }
     }
 
-    public void MakeTriggersForObstacleColliders()
+    private void MakeTriggerForCurrentCollider()
     {
-        GameObject[] obstacleColliders = GameObject.FindGameObjectsWithTag("Obstacle_Collider");
-        foreach (GameObject obstacleColliderObject in obstacleColliders)
+        // Set the current collider as trigger
+        obstacleCollider.isTrigger = true;
+
+        // Set the other colliders as non-trigger
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        foreach (Collider2D collider in colliders)
         {
-            Collider2D collider = obstacleColliderObject.GetComponent<Collider2D>();
-            if (collider != null)
+            if (collider != obstacleCollider && collider.CompareTag("Obstacle_Collider"))
             {
-                collider.isTrigger = true;
+                collider.isTrigger = false;
             }
         }
+
         canvas.GetComponent<Preguntas>().HideQuestion();
     }
 
     private void TurnTriggerIntoCollider()
     {
+        // Set the current collider as non-trigger
         obstacleCollider.isTrigger = false;
+
+        // Set the other colliders as non-trigger
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider != obstacleCollider && collider.CompareTag("Obstacle_Collider"))
+            {
+                collider.isTrigger = false;
+            }
+        }
+
         canvas.GetComponent<Preguntas>().HideQuestion();
     }
 
@@ -115,8 +202,18 @@ public class Obstacle : MonoBehaviour
             preguntas.ButtonPressed -= OnTriggerDecision;
         }
     }
+
     public void Restart()
     {
         obstacleCollider.isTrigger = true;
+
+        Collider2D[] colliders = GetComponents<Collider2D>();
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider != obstacleCollider && collider.CompareTag("Obstacle_Collider"))
+            {
+                collider.isTrigger = false;
+            }
+        }
     }
 }
