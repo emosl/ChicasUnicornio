0bf0992e67eee724d1d91b939f893a43854c7ec2
Emilia Salazar <emiliasalazar67@gmail.com>
2023-04-24 13:43:21 -0600

    abersi

--- Wings_of_Glory/Assets/Items and Inventory/Scripts/Character.cs
+++ Wings_of_Glory/Assets/Items and Inventory/Scripts/Character.cs
@@ -81,4 +81,6 @@ private void Awake()
     }
 
     
+
+    
 }
---
+++ Wings_of_Glory/Assets/Items and Inventory/Scripts/Item_Obj.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Obj : MonoBehaviour
+{
+    public EquippableItem equippableItem;
+}
