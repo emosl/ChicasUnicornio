ab5820361fd8f3440809040c910de56a8686391f
Emilia Salazar <emiliasalazar67@gmail.com>
2023-04-26 12:29:03 -0600

    Si SE PUDO AJUA

--- Wings_of_Glory/Assets/Scripts/gadgets.cs
+++ Wings_of_Glory/Assets/Scripts/gadgets.cs
@@ -1,11 +1,80 @@
 
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class gadgets : MonoBehaviour
+
+// {
+//     public EquippableItem equippableItem;
+//     public GameObject player;
+//     public GameObject[] invisible;
+//     public Inventory inventory;
+//     public ItemPickupPanel panel;
+//     public InventoryInput inventoryInput;
+   
+
+//     void Start()
+//     {
+//         player = GameObject.FindGameObjectWithTag("Player");
+//         inventory = player.GetComponent<Character>().inventory;
+//         equippableItem.RandomizeBonuses(equippableItem.ItemName);
+//         panel = GameObject.Find("ItemPickupPanel").GetComponent<ItemPickupPanel>();
+//         inventoryInput = FindObjectOfType<InventoryInput>();
+        
+//     } 
+
+
+// public void disappeargadgets()
+// {  
+//     //panel.Show(equippableItem);
+//     inventory.AddItem(equippableItem);
+//     Destroy(gameObject);  
+      
+// }
+
+
+// //     void Update()
+// // {
+// //     if (Input.GetKeyDown(KeyCode.E))
+// //     {   
+// //          panel.Hide();
+         
+         
+// //         inventoryInput.CharacterPanelGameObject.SetActive(true); 
+        
+        
+       
+         
+        
+// //     }
+// //     else if (Input.GetKeyDown(KeyCode.U))
+// //     {    
+// //         inventory.RemoveItem(equippableItem);
+// //         panel.Hide();
+        
+           
+// //     }
+//    void Update()
+//    {
+//     if (Input.GetKeyDown(KeyCode.C))
+//     {   
+//          panel.Hide();    
+//     }
+
+//    }
+// }
+
+
+
+    
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class gadgets : MonoBehaviour
-
 {
     public EquippableItem equippableItem;
     public GameObject player;
@@ -13,7 +82,6 @@ public class gadgets : MonoBehaviour
     public Inventory inventory;
     public ItemPickupPanel panel;
     public InventoryInput inventoryInput;
-   
 
     void Start()
     {
@@ -22,50 +90,19 @@ public class gadgets : MonoBehaviour
         equippableItem.RandomizeBonuses(equippableItem.ItemName);
         panel = GameObject.Find("ItemPickupPanel").GetComponent<ItemPickupPanel>();
         inventoryInput = FindObjectOfType<InventoryInput>();
-        
-    } 
-
-
-public void disappeargadgets()
-{  
-    //panel.Show(equippableItem);
-    inventory.AddItem(equippableItem);
-    Destroy(gameObject);  
-      
-}
-
-
-//     void Update()
-// {
-//     if (Input.GetKeyDown(KeyCode.E))
-//     {   
-//          panel.Hide();
-         
-         
-//         inventoryInput.CharacterPanelGameObject.SetActive(true); 
-        
-        
-       
-         
-        
-//     }
-//     else if (Input.GetKeyDown(KeyCode.U))
-//     {    
-//         inventory.RemoveItem(equippableItem);
-//         panel.Hide();
-        
-           
-//     }
-   void Update()
-   {
-    if (Input.GetKeyDown(KeyCode.C))
-    {   
-         panel.Hide();    
     }
 
-   }
+    public void disappeargadgets()
+    {
+        inventory.AddItem(equippableItem);
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            panel.Hide();
+        }
+    }
 }
-
-
-
-    
