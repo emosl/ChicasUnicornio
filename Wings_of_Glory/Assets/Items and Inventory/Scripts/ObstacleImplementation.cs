9167d020e2e80546fe0d3af4f131ecc9058b58dd
Emilia Salazar <emiliasalazar67@gmail.com>
2023-04-26 20:58:37 -0600

    canvas obstacles

--- Wings_of_Glory/Assets/Items and Inventory/Scripts/ObstacleImplementation.cs
+++ Wings_of_Glory/Assets/Items and Inventory/Scripts/ObstacleImplementation.cs
@@ -71,13 +71,16 @@ public class ObstacleImplementation : MonoBehaviour
 {
     private TriggerValueAssigner triggerValueAssigner;
 
-    // public GameObject itemRemovedPanel;
-    // public GameObject itemNotRemovedPanel;
+    public GameObject itemRemovedPanel;
+    public GameObject itemNotRemovedPanel;
 
 
     private void Start()
     {
         triggerValueAssigner = GetComponent<TriggerValueAssigner>();
+
+         itemRemovedPanel.SetActive(false);
+        itemNotRemovedPanel.SetActive(false);
         
     }
 
@@ -91,108 +94,105 @@ public class ObstacleImplementation : MonoBehaviour
         }
     }
 
-    private void CheckPlayerStats(Toby toby)
-    {
-        switch (triggerValueAssigner.triggerTag)
-        {
-            case TriggerValueAssigner.TriggerTag.Ice:
-            Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.agility);
-
-                if (toby.agility < triggerValueAssigner.requiredValue)
-                {
-                    toby.RemoveItem("Agility");
-                }
-                break;
-            case TriggerValueAssigner.TriggerTag.Bomb:
-            Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.shield);
-
-                if (toby.shield < triggerValueAssigner.requiredValue)
-                {
-                    toby.RemoveItem("Shield");
-                }
-                break;
-            case TriggerValueAssigner.TriggerTag.Water:
-            Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.speed);
-
-                if (toby.speed < triggerValueAssigner.requiredValue)
-                {
-                    toby.RemoveItem("Speed");
-                }
-                break;
-            case TriggerValueAssigner.TriggerTag.Rock:
-            Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.strength);
-
-                if (toby.strength < triggerValueAssigner.requiredValue)
-                {
-                    toby.RemoveItem("Strength");
-                }
-                break;
-        }
-    }
     // private void CheckPlayerStats(Toby toby)
     // {
     //     switch (triggerValueAssigner.triggerTag)
     //     {
     //         case TriggerValueAssigner.TriggerTag.Ice:
+    //         Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.agility);
+
     //             if (toby.agility < triggerValueAssigner.requiredValue)
     //             {
     //                 toby.RemoveItem("Agility");
-
-    //                 if (toby.agility < triggerValueAssigner.requiredValue)
-    //                 {
-    //                     itemRemovedPanel.SetActive(true);
-    //                 }
-    //                 else
-    //                 {
-    //                     itemNotRemovedPanel.SetActive(true);
-    //                 }
     //             }
     //             break;
     //         case TriggerValueAssigner.TriggerTag.Bomb:
+    //         Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.shield);
+
     //             if (toby.shield < triggerValueAssigner.requiredValue)
     //             {
     //                 toby.RemoveItem("Shield");
-
-    //                 if (toby.shield < triggerValueAssigner.requiredValue)
-    //                 {
-    //                     itemRemovedPanel.SetActive(true);
-    //                 }
-    //                 else
-    //                 {
-    //                     itemNotRemovedPanel.SetActive(true);
-    //                 }
     //             }
     //             break;
     //         case TriggerValueAssigner.TriggerTag.Water:
+    //         Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.speed);
+
     //             if (toby.speed < triggerValueAssigner.requiredValue)
     //             {
     //                 toby.RemoveItem("Speed");
-
-    //                 if (toby.speed < triggerValueAssigner.requiredValue)
-    //                 {
-    //                     itemRemovedPanel.SetActive(true);
-    //                 }
-    //                 else
-    //                 {
-    //                     itemNotRemovedPanel.SetActive(true);
-    //                 }
     //             }
     //             break;
     //         case TriggerValueAssigner.TriggerTag.Rock:
+    //         Debug.Log("Checking " + triggerValueAssigner.triggerTag + " - Required value: " + triggerValueAssigner.requiredValue + " - Player value: " + toby.strength);
+
     //             if (toby.strength < triggerValueAssigner.requiredValue)
     //             {
     //                 toby.RemoveItem("Strength");
-
-    //                 if (toby.strength < triggerValueAssigner.requiredValue)
-    //                 {
-    //                     itemRemovedPanel.SetActive(true);
-    //                 }
-    //                 else
-    //                 {
-    //                     itemNotRemovedPanel.SetActive(true);
-    //                 }
     //             }
     //             break;
     //     }
     // }
+    private void CheckPlayerStats(Toby toby)
+    {
+        switch (triggerValueAssigner.triggerTag)
+        {
+            case TriggerValueAssigner.TriggerTag.Ice:
+                if (toby.agility < triggerValueAssigner.requiredValue)
+                {
+                    toby.RemoveItem("Agility");
+                    itemRemovedPanel.SetActive(true);   
+                }
+                else
+                {
+                    itemNotRemovedPanel.SetActive(true);
+
+                }
+                break;
+            case TriggerValueAssigner.TriggerTag.Bomb:
+                if (toby.shield < triggerValueAssigner.requiredValue)
+                {
+                    toby.RemoveItem("Shield");
+
+                    if (toby.shield < triggerValueAssigner.requiredValue)
+                    {
+                        itemRemovedPanel.SetActive(true);
+                    }
+                    else
+                    {
+                        itemNotRemovedPanel.SetActive(true);
+                    }
+                }
+                break;
+            case TriggerValueAssigner.TriggerTag.Water:
+                if (toby.speed < triggerValueAssigner.requiredValue)
+                {
+                    toby.RemoveItem("Speed");
+
+                    if (toby.speed < triggerValueAssigner.requiredValue)
+                    {
+                        itemRemovedPanel.SetActive(true);
+                    }
+                    else
+                    {
+                        itemNotRemovedPanel.SetActive(true);
+                    }
+                }
+                break;
+            case TriggerValueAssigner.TriggerTag.Rock:
+                if (toby.strength < triggerValueAssigner.requiredValue)
+                {
+                    toby.RemoveItem("Strength");
+
+                    if (toby.strength < triggerValueAssigner.requiredValue)
+                    {
+                        itemRemovedPanel.SetActive(true);
+                    }
+                    else
+                    {
+                        itemNotRemovedPanel.SetActive(true);
+                    }
+                }
+                break;
+        }
+    }
 }
