e93dd715fa1108761d30690da6aac2a281fc6b37
Arantza Parra <A01782023@tec.mx>
2023-04-28 03:05:29 -0600

    Created enemy_behaviour for fight

--- Wings_of_Glory/Assets/Scripts/TobyFighting.cs
+++ Wings_of_Glory/Assets/Scripts/TobyFighting.cs
@@ -1,4 +1,4 @@
-// joleping
+// Toby movements and animation in during the fight
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,26 +16,13 @@ public class Toby_stats2
 public class TobyFighting : MonoBehaviour
 {
     private Rigidbody2D rb;
-    //private Preguntas ButtonPressed;
-    //private Preguntas button;
-    //private Obstacle obstacleCollider;
-    //private Obstacle obstacle;
     private SpriteRenderer spriteRenderer;
     public Animator animator;
-    //public Obstacle currentObstacle;
     public GameObject player;  
     public Transform objectToFollow;
     
 
 public float speed = 20f;
-
-    //public float jumpForce = 10f;
-
-    //public float shield=0f;
-
-    //public float agility=0f;
-
-
     public float groundCheckRadius = 1f;
     public LayerMask groundLayerMask;
 
@@ -47,7 +34,6 @@ public float speed = 20f;
     //public GameObject[] levels;
 
     private Toby_stats2 toby_stats2 = new Toby_stats2();
-    //public Vector3 initialPosition2; // added variable to store initial position
 
     public Vector3 initialPosition2; // added variable to store initial position
 
@@ -59,10 +45,6 @@ public float speed = 20f;
         animator.Play("idle");
         toby_stats2.initialPosition2 = transform.position;
         toby_stats2.savedPosition2 = transform.position;
-        //button = FindObjectOfType<Preguntas>();
-        //obstacleCollider = FindObjectOfType<Obstacle>();
-
-         // save the initial position of the sprite
         string jsonStats = PlayerPrefs.GetString("toby_stats", JsonUtility.ToJson(toby_stats2));
         Debug.Log(jsonStats);
         toby_stats2 = JsonUtility.FromJson<Toby_stats2>(jsonStats);
@@ -81,9 +63,6 @@ public float speed = 20f;
         Bounds bounds = GetComponent<Collider2D>().bounds;
         Vector2 offset = new Vector2(0f, -bounds.extents.y);
         isGrounded = Physics2D.OverlapCircle((Vector2)transform.position + offset, groundCheckRadius, groundLayerMask);
-
-    // Collider2D barrier = Physics2D.Overlapbox(destination, Vector2.zero, 0f, LayerMask.GetMask("Barrier"));
-    // Collider2D barrier = Physics2D.OverlapArea(destination - Vector3.one * 0.5f, destination + Vector3.one * 0.5f, LayerMask.GetMask("Barrier"));
 
         // Move left or right based on user input
         if (Input.GetKey(KeyCode.LeftArrow))
