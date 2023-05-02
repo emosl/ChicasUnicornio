77bc92ff8554ac0485ff55d6110b36f1ad8a8bc6
Emilia Salazar <emiliasalazar67@gmail.com>
2023-05-01 21:08:09 -0600

    mundos

---
+++ Wings_of_Glory/Assets/Items and Inventory/Scripts/TobyStartPosition.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TobyStartPosition : MonoBehaviour
+{
+    public List<SpriteRenderer> startPositionOptions;
+    public GameObject toby;
+
+    void Start()
+    {
+        // Choose a random SpriteRenderer from the list of options
+        SpriteRenderer startPosition = startPositionOptions[Random.Range(0, startPositionOptions.Count)];
+        
+        // Set Toby's position to the position of the selected SpriteRenderer
+        toby.transform.position = startPosition.transform.position;
+    }
+}
